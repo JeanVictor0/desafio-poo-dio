74900c908e59a2acd5f7102b11989f43ce39be8e
Jean Victor <59372188+JeanVictor0@users.noreply.github.com>
2022-07-19 21:27:23 -0300

    Update: classes de dominio

--- src/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -10,6 +10,36 @@ public class Bootcamp {
 	private String descricao;
 	private final LocalDate data = LocalDate.now();
 	private final LocalDate dataFinal = data.plusDays(75);
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Set<Dev> getDevsInscritos() {
+		return devsInscritos;
+	}
+	public void setDevsInscritos(Set<Dev> devsInscritos) {
+		this.devsInscritos = devsInscritos;
+	}
+	public Set<Conteudo> getConteudos() {
+		return conteudos;
+	}
+	public void setConteudos(Set<Conteudo> conteudos) {
+		this.conteudos = conteudos;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public LocalDate getDataFinal() {
+		return dataFinal;
+	}
 	private Set<Dev> devsInscritos = new HashSet<>();
 	private Set<Conteudo> conteudos = new LinkedHashSet<>();
 	
